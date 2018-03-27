555fd5ee7814f3044877c71ec4daed0366796bd7
Cheng Gu <gucheng@google.com>
2018-03-26 22:58:18 -0700

    misc: easelcomm: Increase local ringbuffer size
    Increase local cmd channel size to 1 MB.
    
    Change-Id: I6cf525c6addc4b0e0a207bf2a11dfb6d7375a21d
    Signed-off-by: Cheng Gu <gucheng@google.com>

--- drivers/misc/google-easel-comm-shared.h
+++ drivers/misc/google-easel-comm-shared.h
@@ -36,8 +36,8 @@
 /* Remote command channel buffer size 100 KB */
 #define EASELCOMM_CMD_CHANNEL_REMOTE_SIZE	(100 * 1024)
 
-/* Local command channel buffer size 100 KB */
-#define EASELCOMM_CMD_CHANNEL_LOCAL_SIZE	(100 * 1024)
+/* Local command channel buffer size 1 MB */
+#define EASELCOMM_CMD_CHANNEL_LOCAL_SIZE	(1024 * 1024)
 
 /*
  * Command channel header at the top of each command channel buffer contains
